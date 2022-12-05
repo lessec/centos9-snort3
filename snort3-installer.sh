1269ba5a08d0be6f3b85bf74f8a3f689151f3fb6
Leslie <lesysec@gmail.com>
2022-12-05 18:48:50 +0900

    Fix error

--- snort3-installer.sh
+++ snort3-installer.sh
@@ -5,19 +5,38 @@
 sudo ldconfig
 mkdir -p ~/sources
 
+
 ## Basic Dependencies
 sudo dnf install -y curl git vim flex bison gcc gcc-c++ make cmake automake autoconf libtool libpcap-devel pcre-devel libdnet-devel hwloc-devel openssl-devel zlib-devel luajit-devel pkgconf libmnl-devel libunwind-devel libnfnetlink-devel libnetfilter_queue-devel xz-devel libuuid-devel hyperscan hyperscan-devel gperftools-devel
+
 git clone https://github.com/snort3/libdaq.git ~/sources/libdaq
 cd ~/sources/libdaq
 sudo ./bootstrap
 sudo ./configure --disable-netmap-module --disable-divert-module
-sudo make && sudo make install && sudo ldconfig
-cd ~/sources && curl -Lo flatbuffers-22.11.23.tar.gz https://github.com/google/flatbuffers/archive/v22.11.23.tar.gz && tar xf flatbuffers-22.11.23.tar.gz
+sudo make
+sudo make install
+sudo ln -s /usr/local/lib/libdaq.so.3 /lib
+sudo ldconfig
+
+cd ~/sources
+curl -Lo flatbuffers-22.11.23.tar.gz https://github.com/google/flatbuffers/archive/v22.11.23.tar.gz
+tar xf flatbuffers-22.11.23.tar.gz
 mkdir -p ~/sources/fb-build
 cd ~/sources/fb-build
-sudo cmake ~/sources/flatbuffers-22.11.23 && sudo make -j$(nproc) && sudo make -j$(nproc) install && sudo ldconfig && flatc --version
-cd ~/sources && curl -Lo cert-forensics-tools-release-el9.rpm https://forensics.cert.org/cert-forensics-tools-release-el9.rpm && sudo rpm -Uvh cert-forensics-tools-release*rpm && sudo dnf --enablerepo=forensics install -y libsafec libsafec-devel
-sudo ln -s /usr/lib64/pkgconfig/safec-3.3.pc /usr/lib64/pkgconfig/libsafec.pc && sudo ln -s /usr/lib64/libtcmalloc.so.4 /lib/ && sudo ln -s /usr/local/lib/libdaq.so.3 /lib/ && sudo ldconfig
+sudo cmake ~/sources/flatbuffers-22.11.23
+sudo make -j$(nproc)
+sudo make -j$(nproc) install
+sudo ldconfig
+flatc --version
+
+cd ~/sources
+curl -Lo cert-forensics-tools-release-el9.rpm https://forensics.cert.org/cert-forensics-tools-release-el9.rpm
+sudo rpm -Uvh cert-forensics-tools-release*rpm
+sudo dnf --enablerepo=forensics install -y libsafec libsafec-devel
+sudo ln -s /usr/lib64/pkgconfig/safec-3.3.pc /usr/lib64/pkgconfig/libsafec.pc
+sudo ln -s /usr/lib64/libtcmalloc.so.4 /lib
+sudo ldconfig
+
 
 ## Install Snort3
 git clone https://github.com/snort3/snort3.git ~/sources/snort3
