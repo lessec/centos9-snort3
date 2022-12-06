561add46bf9489ff0f8a948691a84b5504fe5e71
Leslie <lesysec@gmail.com>
2022-12-06 10:09:25 +0900

    Add PulledPork3 Oinkcode

--- pulledpork3-installer.sh
+++ pulledpork3-installer.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # CentOS 9 Stream with Snort3 Auto update through PulledPork3
+ORGNPATH=$(pwd)
 
 ## Dependencies
 sudo dnf install -y python3 python3-pip python3-devel
@@ -17,10 +18,13 @@ sudo cln -s /usr/local/pulledpork/pulledpork.py /usr/bin/pulledpork
 pulledpork -V
 
 ## Configure PulledPork3
+read -p "Enter Oinkcode: " OINKCODE
+sudo cp "$ORGNPATH"/cfg/pulledpork3.conf "$ORGNPATH"/cfg/pulledpork.conf
+echo 'oinkcode = "$OINKCODE"\n' >> "$ORGNPATH"/cfg/pulledpork.conf
 sudo mv /usr/local/pulledpork/etc/pulledpork.conf /usr/local/pulledpork/etc/pulledpork.conf.default
-sudo cp ./cfg/pulledpork3.conf /usr/local/pulledpork/etc/pulledpork.conf
-sudo cp ./cfg/pulledpork.service /etc/systemd/system
-sudo cp ./cfg/pulledpork.timer /etc/systemd/system
+sudo mv "$ORGNPATH"/cfg/pulledpork.conf /usr/local/pulledpork/etc/pulledpork.conf
+sudo cp "$ORGNPATH"/cfg/pulledpork.service /etc/systemd/system
+sudo cp "$ORGNPATH"/cfg/pulledpork.timer /etc/systemd/system
 sudo touch /usr/local/pulledpork/etc/enablesid.conf
 sudo touch /usr/local/pulledpork/etc/dropsid.conf
 sudo touch /usr/local/pulledpork/etc/disablesid.conf
