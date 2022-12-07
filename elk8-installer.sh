80977da59534aedb62ae6615c3781d1db5751674
Leslie <lesysec@gmail.com>
2022-12-07 13:57:30 +0900

    Make short

--- elk8-installer.sh
+++ elk8-installer.sh
@@ -6,14 +6,12 @@ sudo -Sv
 ORGNPATH=$(pwd)
 sudo dnf install -y java-17-openjdk java-17-openjdk-devel
 
-## Setup Elasticsearch, Logstash and Kibana
+## Install Elasticsearch, Logstash and Kibana
 sudo update-crypto-policies --set LEGACY
 sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 sudo cp "$ORGNPATH"/cfg/elasticsearch8.repo /etc/yum.repos.d/elasticsearch.repo
 sudo cp "$ORGNPATH"/cfg/logstash8.repo /etc/yum.repos.d/logstash.repo
 sudo cp "$ORGNPATH"/cfg/kibana8.repo /etc/yum.repos.d/kibana.repo
-
-## Install Elasticsearch, Logstash and Kibana
 sudo dnf --enablerepo=elasticsearch install -y elasticsearch logstash kiban
 
 ## Install Filebeat
